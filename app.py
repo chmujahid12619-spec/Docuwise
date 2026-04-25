9d2ec42c3ca005d5501c263fdc7a000855c74b0e
chmujahid12619-spec <chmujahid12619@gmail.com>
2026-04-25 07:40:26 +0500

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ st.set_page_config(page_title="Docuwise", page_icon="📄")
 st.title("📄 Docuwise - PDF سے بات کریں")
 st.write("اپنی PDF فائل اپلوڈ کریں اور اس سے سوال پوچھیں")
 
-st.file_uploader("PDF فائل چنیں", type="pdf")
+ooplodad-file =st.file_uploader("PDF فائل چنیں", type="pdf")
 
 if uploaded_file is not None:
     pdf_reader = PdfReader(uploaded_file)
@@ -18,9 +18,9 @@ if uploaded_file is not None:
     
     question = st.text_input("PDF کے بارے میں سوال پوچھیں:")
     
-    if question: gv
+    if question: 
         st.write("*جواب:*")
-        if question.lower() in text.lower():v
+        if question.lower() in text.lower():
             st.info("آپ کا سوال PDF میں موجود ہے۔")
         else:
             st.warning("معذرت، یہ معلومات PDF میں نہیں ملی۔")
